2c17eb4077fd4762a98630ef945263dd6fe337ef
Sigitas <sigitaslt1999@gmail.com>
2019-11-05 00:27:57 +0200

    First commit

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,10 +1,48 @@
 package com.company;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+
+class Main extends Canvas implements Runnable {
+
+    public static final int PLOTIS = 1366, AUKSTIS = 768;
+    private boolean vykdymas = false;
+    private  Thread gija;
+
+    public Main(){
+        Dimension matmenys = new Dimension(Main.PLOTIS, Main.AUKSTIS);
+        setPreferredSize(matmenys);
+    }
 
     public static void main(String[] args) {
 
+        JFrame kadras = new JFrame("Žaidimas");
+        Main game = new Main();
+        kadras.add(game);
 
+        kadras.setResizable(false);
+        kadras.pack();
+
+        kadras.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        kadras.setLocationRelativeTo(null);
+        kadras.setVisible(true);
+
+
+    }
+
+    //Runnable metodas:
+    @Override
+    public void run() {
+        update();
+        render();
+
+    }
+
+    private void update() {
+
+    }
+
+    private void render() {
 
     }
 }
